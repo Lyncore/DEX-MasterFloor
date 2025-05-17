d91d5e403b6f572552d6a2fd47b328514e8c551f
Lyncore <iandroid444@gmail.com>
2025-05-17 13:16:25 +0700

    code upload

--- MasterPol/MainWindow.xaml.cs
+++ MasterPol/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MasterPol.Models;
+using Microsoft.EntityFrameworkCore;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MasterPol
@@ -23,11 +24,38 @@ namespace MasterPol
             InitializeComponent();
             LoadData();
         }
-
+        /// <summary>
+        /// Загрузка данных
+        /// </summary>
         void LoadData()
         {
-            using var context = new Wsr1DexDemoContext();
-            this.ProductType.ItemsSource = context.ProductTypes.ToArray();
+            try
+            {
+                using var context = new Wsr1DexDemoContext();
+
+                var mapped = context.Partners
+                    .Include(p => p.Histories)
+                    .Select(p => new {
+                        p.Id,
+                        p.Type,
+                        p.Name,
+                        p.DirectorName,
+                        p.DirectorPhone,
+                        p.Rank,
+                        Discount = $"{DiscountHelper.Calculate(p.Histories.Sum(p => p.Count))} %"
+
+                    })
+                    .ToArray();
+
+                Partners.ItemsSource = mapped;
+            } catch (Exception ex) {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void Partners_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
